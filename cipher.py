b79f12ec7ddb320f57b4dd8c7e71595ca4c1b08d
gorm <gormleyethan@gmail.com>
2023-09-23 11:53:09 -0700

    code refactored

--- cipher.py
+++ cipher.py
@@ -1,45 +1,32 @@
-#Ethanb Gormley
-#Pad cipher using user input for CIT 55510
-#program was written in C# iniially since I write in it at work
-#but i do not have .net installed on this mac and I do not
-#feel like configuring it
-
-#import random and string so it can generate readable characters
 import random
 import string
 
-#generate the key according to how big the plaintext is
+
 def generate_key(plaintext):
-    key = ''.join(random.choices(string.ascii_uppercase, k=len(plaintext)))
-    return key
+    """Generate a random key based on the length of the plaintext."""
+    return ''.join(random.choices(string.ascii_uppercase, k=len(plaintext)))
 
-#encrypt the text using the user provided text and generated key
+
 def encrypt(plaintext, key):
-    ciphertext = ""
-    for i, char in enumerate(plaintext):
-        ciphertext += chr((ord(char) + ord(key[i % len(key)])) % 26 + 65)
-    return ciphertext
+    """Encrypt the plaintext using the provided key."""
+    return ''.join(chr((ord(char) + ord(key[i % len(key)])) % 26 + 65) for i, char in enumerate(plaintext))
 
-#decrypt everthing by doing what i basically did backwards
+
 def decrypt(ciphertext, key):
-    plaintext = ""
-    for i, char in enumerate(ciphertext):
-        plaintext += chr((ord(char) - ord(key[i % len(key)])) % 26 + 65)
-    return plaintext
-
-#the heart of the program
-plaintext = input("Enter plaintext: ")
-plaintext = plaintext.upper()
-key = generate_key(plaintext)
-ciphertext = encrypt(plaintext, key)
-
-#the soul
-print("Plaintext: ", plaintext)
-print("Key: ", key)
-print("Ciphertext: ", ciphertext)
-print("Decrypted Text: ", decrypt(ciphertext, key))
+    """Decrypt the ciphertext using the provided key."""
+    return ''.join(chr((ord(char) - ord(key[i % len(key)])) % 26 + 65) for i, char in enumerate(ciphertext))
 
 
-    #upload to github and put link on pdf for refrence
-    #git broken
-    #test
+def main():
+    plaintext = input("Enter plaintext: ").upper()
+    key = generate_key(plaintext)
+    ciphertext = encrypt(plaintext, key)
+
+    print(f"Plaintext: {plaintext}")
+    print(f"Key: {key}")
+    print(f"Ciphertext: {ciphertext}")
+    print(f"Decrypted Text: {decrypt(ciphertext, key)}")
+
+
+if __name__ == "__main__":
+    main()
